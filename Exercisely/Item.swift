124f19a44ac98eac2da597d6339260eb3828052c
Jason Vance <jasonvance77@gmail.com>
2025-01-07 21:05:26 -1000

    Initial Commit

---
+++ Exercisely/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Exercisely
+//
+//  Created by Jason Vance on 1/7/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
